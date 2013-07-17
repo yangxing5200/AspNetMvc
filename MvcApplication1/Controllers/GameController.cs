4901f8155d8ebaf1f93094f33d4f21c0ddbbb758
yx1990 <418505093@qq.com>
2013-07-17 20:56:49 +0800

    添加了一个控制器，在控制器中添加了一个方法（返回值为一直字符串）

---
+++ MvcApplication1/Controllers/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class GameController : Controller
+    {
+        //
+        // GET: /Game/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string GetIndex()
+        {
+            return "执行了 GameController GetIndex()";
+        }
+
+    }
+}
