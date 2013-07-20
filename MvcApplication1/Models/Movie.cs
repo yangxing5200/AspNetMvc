5cfd35fd394c0efda92de50b8af87f8a42edcdd3
yx1990 <418505093@qq.com>
2013-07-20 21:23:23 +0800

    新建一个Model的数据文件，上下文文件后使用编译器添加控制器和视图 ，生成对应的增删改查页面。

---
+++ MvcApplication1/Models/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models
+{
+    public class DbContextMovie:DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+    public class Movie
+    {
+        [Required]
+        [DataType(DataType.CreditCard)]
+        [Display(Name = "影片编号")]
+        public int Id { get; set; }
+
+    
+
+        [Required]
+        [StringLength(30)]
+        [Display(Name = "影片名称")]
+        public string Name { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Display(Name = "价格")]
+        public decimal Price { get; set; }
+    }
+}
